lambda-testing/seqan

commit 64a8b76e7ae6a8ccf7df463f66cbdf6ede99d7f6

[fix] value() as well

core/include/seqan/modifier/modifier_view.h
@@ -418,6 +418,26 @@ value(ModifiedString<THost, ModView<TFunctor> > const & me, TPos pos)
 }
 
 // --------------------------------------------------------------------------
+// Function value()                                       [ModifiedString]
+// --------------------------------------------------------------------------
+
+template <typename THost, typename TFunctor, typename TSpec, typename TPos>
+inline typename Reference<Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> >::Type
+value(Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> & me, TPos pos)
+{
+    host(me).tmp_value = cargo(host(me)).func(*(begin(host(me), Standard()) + pos));
+    return host(me).tmp_value;
+}
+
+template <typename THost, typename TFunctor, typename TSpec, typename TPos>
+inline typename Reference<Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> const>::Type
+value(Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> const & me, TPos pos)
+{
+    host(me).tmp_value = cargo(host(me)).func(*(begin(host(me), Standard()) + pos));
+    return host(me).tmp_value;
+}
+
+// --------------------------------------------------------------------------
 // Function getValue()                                       [ModifiedString]
 // --------------------------------------------------------------------------
 

commit 18ef5080ca69e2040301706c8d2d43ff07f257a2

[fix] undo 64a8b76e7ae6a8ccf7df463f66cbdf6ede99d7f6 which broke double indexing

core/include/seqan/modifier/modifier_view.h
@@ -421,21 +421,21 @@ value(ModifiedString<THost, ModView<TFunctor> > const & me, TPos pos)
 // Function value()                                       [ModifiedString]
 // --------------------------------------------------------------------------
 
-template <typename THost, typename TFunctor, typename TSpec, typename TPos>
-inline typename Reference<Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> >::Type
-value(Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> & me, TPos pos)
-{
-    host(me).tmp_value = cargo(host(me)).func(*(begin(host(me), Standard()) + pos));
-    return host(me).tmp_value;
-}
-
-template <typename THost, typename TFunctor, typename TSpec, typename TPos>
-inline typename Reference<Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> const>::Type
-value(Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> const & me, TPos pos)
-{
-    host(me).tmp_value = cargo(host(me)).func(*(begin(host(me), Standard()) + pos));
-    return host(me).tmp_value;
-}
+// template <typename THost, typename TFunctor, typename TSpec, typename TPos>
+// inline typename Reference<Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> >::Type
+// value(Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> & me, TPos pos)
+// {
+//     host(me).tmp_value = cargo(host(me)).func(*(begin(me, Standard()) + pos));
+//     return host(me).tmp_value;
+// }
+// 
+// template <typename THost, typename TFunctor, typename TSpec, typename TPos>
+// inline typename Reference<Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> const>::Type
+// value(Segment<ModifiedString<THost, ModView<TFunctor> >, TSpec> const & me, TPos pos)
+// {
+//     host(me).tmp_value = cargo(host(me)).func(*(begin(me, Standard()) + pos));
+//     return host(me).tmp_value;
+// }
 
 // --------------------------------------------------------------------------
 // Function getValue()                                       [ModifiedString]